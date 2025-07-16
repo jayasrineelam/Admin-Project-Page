9e48bc3e4a16f1a571009bab7be43888ca7daea8
jayasrineelam <jayasri.neelam@gmail.com>
2025-07-15 23:05:04 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProfilePage1MUI from './Components/ProfilePage1MUI';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ProfilePage1MUI/>
+    </div>
+  );
+}
+
+export default App;
